de4678e21f9daef87b09c212e61c7fa801bb2189
TZONE TECH <tzonetech8@gmail.com>
2026-07-10 10:38:56 +0300

    Change email field length from 100 to 50 characters

--- database_setup.sql
+++ database_setup.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
     phone VARCHAR(20) NOT NULL,
     role ENUM('admin', 'manager', 'staff') NOT NULL,
     password VARCHAR(255) NOT NULL,
